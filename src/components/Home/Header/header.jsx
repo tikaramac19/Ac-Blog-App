8fa60a47beca08e4febba8d90e25c111a904c054
tikaramac19 <tikaramac19@gmail.com>
2022-12-01 13:18:15 +0545

    sml chng

--- src/components/Home/Header/header.jsx
+++ src/components/Home/Header/header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import styles from "./header.module.css";
+import Navbar from "./Navbar";
 const Header = () => {
   return (
     <div className={styles.home_header}>
+      {/* navbar import */}
+      {/* <Navbar /> */}
       <h2>Have A Fantastic Day Folks !!</h2>
       <h1>
         <span></span>Ac - Blog <span></span>
