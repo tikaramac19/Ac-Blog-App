806cb309e045b2fcde1266e11d11449cf4de55a2
tikaramac19 <tikaramac19@gmail.com>
2022-12-01 14:03:03 +0545

    if search didnt match category

---
+++ src/components/SharedComponent/emptyComp/EmptyList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.css"
+const EmptyList = () => {
+  return (
+  <>
+  <div className="emptyList-wrap">
+    <img src="/assets/images/13525-empty.gif" alt="" />
+  </div>
+  </>
+  )
+}
+
+export default EmptyList
