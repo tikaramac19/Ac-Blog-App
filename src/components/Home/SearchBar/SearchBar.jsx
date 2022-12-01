a4e164c319857018171dca072b438ddd742d2753
tikaramac19 <tikaramac19@gmail.com>
2022-12-01 12:07:41 +0545

    bg color change

---
+++ src/components/Home/SearchBar/SearchBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+import styles from "./SearchBar.module.css";
+const SearchBar = ({ value, handelSearchKey, formSubmit, clearSearch }) => {
+  return (
+    <div className={styles.searchbar}>
+      <form action="#" onSubmit={formSubmit}>
+        <input
+          type="text"
+          onChange={handelSearchKey}
+          placeholder="Search By Category "
+          value={value}
+        />
+        {value && <span onClick={clearSearch}>X</span>}
+
+        <button>Go</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
