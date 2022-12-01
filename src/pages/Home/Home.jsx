c3015b3f498f0a16e3e55e955f3c7aaa680fc9f2
tikaramac19 <tikaramac19@gmail.com>
2022-12-01 14:04:19 +0545

    search functionity updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,55 @@
-import React from 'react'
-import styles from "./Home.module.css"
-import Header from '../../components/Home/Header/header'
-import SearchBar from '../../components/Home/SearchBar/SearchBar'
-import BlogList from '../../components/Home/BlogList/BlogList'
-import { blogList } from '../../config/data'
-
+import React, { useState } from "react";
+import styles from "./Home.module.css";
+import Header from "../../components/Home/Header/header";
+import SearchBar from "../../components/Home/SearchBar/SearchBar";
+import BlogList from "../../components/Home/BlogList/BlogList";
+import { blogList } from "../../config/data";
+import EmptyList from "../../components/SharedComponent/emptyComp/EmptyList";
 const Home = () => {
+  const [blogs, setBlogs] = useState(blogList);
+  const [searchKey, setSearchKey] = useState("");
+  // console.log(searchKey, "kera mula")
+  // search submit
+
+  const handelSearchSubmit = (e) => {
+    e.preventDefault();
+
+    handleSearchResults();
+  };
+
+  // search for blogs by categories
+  const handleSearchResults = () => {
+    const allBlogs = blogList;
+
+    const filteredBlogs = allBlogs.filter((blog) =>
+      blog.category.toLowerCase().includes(searchKey)
+    );
+
+    setBlogs(filteredBlogs);
+  };
+  const handleClearSearch = () => {
+    setBlogs(blogList);
+    setSearchKey("");
+  };
   return (
     <div className={styles.home_container}>
-        
-    {/* page header */}
+      {/* page header */}
 
-    <Header />
+      <Header />
 
+      {/* search bar */}
 
-    {/* search bar */}
+      <SearchBar
+        value={searchKey}
+        clearSearch={handleClearSearch}
+        formSubmit={handelSearchSubmit}
+        handelSearchKey={(e) => setSearchKey(e.target.value)}
+      />
 
-    <SearchBar />
-
-    {/* BLog list and empty list */}
-
-    <BlogList  blogs={blogList}/>
-
+      {/* BLog list and empty list */}
+      {!blogs.length ?<EmptyList /> : <BlogList blogs={blogs} />}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
